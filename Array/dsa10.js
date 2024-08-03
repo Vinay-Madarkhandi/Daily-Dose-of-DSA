bcf2682f14262c02bd455e3905b77e55e6c657f5
Madarkhandi Vinay Shantappa <vinaymadarkhandi2022@gmail.com>
2024-08-03 16:52:33 +0530

    additon

--- Array/dsa10.js
+++ Array/dsa10.js
@@ -1,6 +1,14 @@
-let n ; //number of 
-let no ;
-let m ; //number of exchnage / exchange rate 
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+  let str = digits.toString().replace(/,/g, "");
+  str = parseInt(str);
+  str = str.toString();
+  str = str.split("");
+  let ans = str.map(Number);
+  return str;
+};
 
-for(let i = 1 ; i )
-
+console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
