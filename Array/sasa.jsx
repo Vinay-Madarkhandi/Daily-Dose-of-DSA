8dda06c8e75755b5f425a5d9ccef26720d024041
Madarkhandi Vinay Shantappa <vinaymadarkhandi2022@gmail.com>
2024-08-11 22:27:36 +0530

    addition

---
+++ Array/sasa.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const sasa = () => {
+  return (
+    <div>
+        <p>
+            <img src=" " alt="" srcset="" />
+            <a href="http://"></a>
+            <div>
+                 <h1>
+                     hello this is the new world !
+                 </h1>
+                <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam libero, est aut dolorum maiores architecto ullam mollitia quisquam similique tenetur repudiandae maxime ducimus cumque perspiciatis quod delectus quam modi rem.
+                </p>
+                <div>
+                    in vs code this is the best thing 
+                </div>
+            </div>
+        </p>
+    </div>
+  )
+}
