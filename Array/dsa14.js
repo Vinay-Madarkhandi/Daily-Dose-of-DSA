d490c326c3f1e99094f8e91a424a0ce83663d10c
Madarkhandi Vinay Shantappa <vinaymadarkhandi2022@gmail.com>
2024-10-08 11:40:10 +0530

    addition

--- Array/dsa14.js
+++ Array/dsa14.js
@@ -1,22 +1,20 @@
+let arr = nums1;
+arr = arr.concat(nums2);
+arr = arr.sort((a, b) => a - b);
+let num1 = nums1[nums1.length - 1];
+let num2 = nums2[0];
+let ans = 0;
+if (arr.length % 2 === 0) {
+  ans = num1 + num2;
+  return (ans = ans / 2);
+}
+if (arr.length % 2 != 0) {
+  return (ans = arr[arr.length / 2]);
+}
+console.log(num1);
+console.log(num2);
+console.log(arr);
+console.log(ans);
 
-  let arr = nums1;
-  arr = arr.concat(nums2);
-  arr = arr.sort((a, b) => a - b);
-  let num1 = nums1[nums1.length - 1];
-  let num2 = nums2[0];
-  let ans = 0;
-  if (arr.length % 2 === 0) {
-    ans = num1 + num2;
-    return (ans = ans / 2);
-  }
-  if (arr.length % 2 != 0) {
-    return (ans = arr[arr.length / 2]);
-  }
-  console.log(num1);
-  console.log(num2);
-  console.log(arr);
-  console.log(ans);
-
-let 
-nums1= [1, 2]
-let nums2=  [3, 4]);
+let nums1 = [1, 2];
+let nums2 = [3, 4];
