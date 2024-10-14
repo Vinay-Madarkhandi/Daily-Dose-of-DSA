846727fcbe1ddfe0829c41754dec896555f8c1c4
Madarkhandi Vinay Shantappa <vinaymadarkhandi2022@gmail.com>
2024-10-14 21:59:32 +0530

    addition

--- ARRAYSN/347.js
+++ ARRAYSN/347.js
@@ -5,9 +5,19 @@
  */
 var topKFrequent = function (nums, k) {
   const map = new Map();
+  let arr = [];
+
   nums.forEach((num) => {
     map.set(num, (map.get(num) || 0) + 1);
   });
+
+  map.forEach((val, key) => {
+    if (val >= k) {
+      arr.push(key);
+    }
+  });
+
+  return arr;
 };
 
 console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
